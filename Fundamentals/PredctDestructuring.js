4d16b8228f3ad53462b0374bc6364d6a01080559
ayat jamhour <engayatjamhour@gmail.com>
2021-07-11 15:40:42 +0300

    fundamentals

--- Fundamentals/PredctDestructuring.js
+++ Fundamentals/PredctDestructuring.js
@@ -1,14 +1,14 @@
-1- 
+1 - 
 const cars = ['Tesla', 'Mercedes', 'Honda']
-const [ randomCar ] = cars
-const [ ,otherRandomCar ] = cars
+const [randomCar] = cars
+const [, otherRandomCar] = cars
 //Predict the output
 console.log(randomCar)
 console.log(otherRandomCar)
 output: "Tesla"
 "Mercedes"
 ....................
-2- 
+2 - 
 const employee = {
     name: 'Elon',
     age: 47,
@@ -18,42 +18,42 @@ const { name: otherName } = employee;
 //Predict the output
 console.log(name);
 console.log(otherName);
-output: "result"
-"Elon"
+// output: Elon
+// undefined //there are no array in this object 
 ....................
-3- 
+3 - 
 const person = {
     name: 'Phil Smith',
     age: 47,
     height: '6 feet'
 }
 const password = '12345';
-const { password: hashedPassword } = person;  
+const { password: hashedPassword } = person;
 //Predict the output
 console.log(password);
 console.log(hashedPassword);
-output: "12345"
-undefined
+// output: "12345"
+// undefined //error
 ....................
-4-
+4 -
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [,first] = numbers;
-const [,,,second] = numbers;
-const [,,,,,,,,third] = numbers;
+const [, first] = numbers;
+const [, , , second] = numbers;
+const [, , , , , , , , third] = numbers;
 //Predict the output
 console.log(first == second);
-console.log(first == third); 
+console.log(first == third);
 output: false
 true
 ....................
-5- 
+5 - 
 const lastTest = {
     key: 'value',
     secondKey: [1, 5, 1, 8, 3, 3]
 }
 const { key } = lastTest;
 const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
+const [, willThisWork] = secondKey;
 //Predict the output
 console.log(key);
 console.log(secondKey);
