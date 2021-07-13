73b9e8d0639762f19aebdf06201b64e528633270
ayat jamhour <engayatjamhour@gmail.com>
2021-07-13 16:21:36 +0300

    together

---
+++ react/putting-it-together/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import CardClick from './components/CardClick.js'
+import React, { Component } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CardClick firstName={"Doe"} lastName={"Jane"} age={45} hairColor={"Black"} />
+      <CardClick firstName={"Smith"} lastName={"John"} age={88} hairColor={"Brown"} />
+      <CardClick firstName={"Fillmore"} lastName={"Millard"} age={50} hairColor={"Brown"} />
+      <CardClick firstName={"Smith"} lastName={"Maria"} age={62} hairColor={"Brown"} />
+    </div>
+  );
+}
+
+export default App;
